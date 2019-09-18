6b8313dd078041054a6c980382ccc460eb75aa89
Nicolò Pasini <eberron@hotmail.it>
2019-09-18 10:02:16 +0200

    Add files via upload

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "OSLogger",
+    platforms: [
+        .iOS(.v10),
+    ],
+    products: [
+        .library(
+            name: "OSLogger",
+            targets: ["OSLogger"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "OSLogger",
+            dependencies: []),
+        .testTarget(
+            name: "OSLoggerTests",
+            dependencies: ["OSLogger"]),
+    ]
+)
